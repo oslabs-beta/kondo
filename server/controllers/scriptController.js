b8b1e6ef976dba71875b42eb3097664360882808
Dakota Gilbreath <dakotag@Dakotas-MacBook-Pro.local>
2020-03-18 01:08:07 -0700

    reworking script storage into json, currently writing works but reading needs work

---
+++ server/controllers/scriptController.js
@@ -0,0 +1,100 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const path = require('path');
+
+// RUN THIS TO RECORD USER ACTIONS FROM BROWSER AND CREATE PUPPET SCRIPT
+exports.createScript = async (name, inputURL) => {
+  console.log('RECORD SCRIPT');
+  const pathToExtension = require('path').join(__dirname, '../../extension');
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`
+    ]
+  });
+  const page = await browser.newPage();
+  await page.goto(inputURL);
+  await page.content();
+  exports.storeScript(name);
+}
+
+// TAKE USER INPUT AND WRITE TO SCRIPT FILE
+exports.storeScript = async (name) => {
+  let input = [];
+
+  console.log('Please enter the function you\'d like to store');
+  rl.prompt();
+
+  rl.on('line', function (line) {
+      input.push(line);
+  })
+
+  rl.on('close', function (cmd) {
+    let answer = input.join('\n');
+      
+    fs.readFile(path.join(__dirname, '../userscripts.json'), function (err, data) {
+      if (err) throw err;
+      let json = JSON.parse(data);
+      json[name] = { 
+          arguments:'page', 
+          body: answer.toString() 
+      }
+      fs.writeFile(path.join(__dirname, '../userscripts.json'), JSON.stringify(json), function(err) {
+        if (err) throw err;
+        console.log('saved!');
+        process.exit(0);
+      })
+    })
+  })
+}
+// may need to set up variables in global memory to get this working
+var AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
+
+// RUN THIS AFTER RECORDING PUPPET SCRIPT
+// launch puppeteer headless and open the page provided by the user
+exports.runScript = async (script, inputURL) => {
+  
+  console.log('RUN SCRIPT');
+  let browser = await puppeteer.launch();
+  let context = await browser
+      .createIncognitoBrowserContext();
+  let page = await context.newPage();
+  await page.goto(inputURL);
+  await page.content();
+
+  
+  let json = fs.readFileSync(path.join(__dirname, '../userscripts.json'))
+  json = JSON.parse(json);
+  // console.log('JSON: ', json);
+  // remove \n before evaluation
+  let body = json[script].body.replace(/\n  /g, ' ');
+  // let asyncCb = new AsyncFunction(json[script].arguments, body);
+  console.log(typeof body);
+  let func = new AsyncFunction;
+  
+  console.log('JSON: ', json);    
+  console.log('FUNC: ', func);
+  // if (json) {
+    let count = 0;
+    // console.log(' json exists ')
+    while (count < 7) {
+      // Do something a couple of times. (insert recorded puppeteer scripts here)
+      // await script(page);
+      // eval(json[script].body);
+      // eval("(async (page) => {" + json + "})()")
+      // eval(json);
+      // console.log('working?')
+      func(page);
+      count++;
+    }
+  // }
+  
+  // context.close();
+  browser.close();
+}
--- server/server.js
+++ server/server.js
@@ -1,66 +1,42 @@
 #!/usr/bin/env node
 const express = require('express');
-const puppeteer = require('puppeteer');
 const app = express();
 const path = require('path');
-const fs = require('fs');
-const scripts = require('./userscripts');
+const action = require('./controllers/scriptController');
+
+// determines whether to save a new script or run an existing one in headless mode
+let runMode = process.argv.slice(2)[0];
+
+// the name of the script to be saved or executed
+let scriptName = process.argv.slice(3)[0];
 
 // take the URL to open in Puppeteer from the input script parameter
-let inputURL = process.argv.slice(2)[0];
-
-// determines whether to get a new script or run an existing one in headless mode
-let runMode = process.argv.slice(3)[0];
-
-// RUN THIS AFTER RECORDING PUPPET SCRIPT
-// launch puppeteer headless and open the page provided by the user
-const runScript = async (script) => {
-  console.log('RUN SCRIPT');
-  let browser = await puppeteer.launch();
-  let context = await browser
-      .createIncognitoBrowserContext();
-  let page = await context.newPage();
-  await page.goto(inputURL);
-  await page.content();
-  
-  let count = 0;
-  while (count < 7) {
-    // Do something a couple of times. (insert recorded puppeteer scripts here)
-    await script(page);
-    count++;
-  }
-  // context.close();
-  browser.close();
-}
-
-// RUN THIS TO RECORD USER ACTIONS FROM BROWSER AND CREATE PUPPET SCRIPT
-const addScript = async () => {
-  console.log('RECORD SCRIPT');
-  const pathToExtension = require('path').join(__dirname, '../extension');
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      `--disable-extensions-except=${pathToExtension}`,
-      `--load-extension=${pathToExtension}`
-    ]
-  });
-  const page = await browser.newPage();
-  await page.goto(inputURL);
-  await page.content();
-}
+let inputURL = process.argv.slice(4)[0];
 
 // handle input parameters
-if (!inputURL) console.log('Please provide a URL to connect to e.g. npm start -- http://localhost:3000 <scriptName(optional)>');
-else {
-  app.listen(3999, () => console.log('kondo connected to server on port 3999'));
-
-  if (!runMode) addScript();
-  else if (runMode === '?') {
-    console.log(Object.keys(scripts));
-  }
-  else {
-    runScript(scripts[runMode]);
-  }
+if (!runMode) {
+  console.log('Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"');
+}
+else switch (runMode) {
+  case 'create':
+    // expect npm start -- create scriptName URL
+    // if scriptName and URL exist, run createScript and write to userscripts.js
+    if (scriptName && inputURL) action.createScript(scriptName, inputURL);
+    // else print syntax explanation
+    else console.log("Please enter the name of the script you'd like to create followed by the page URL.")
+    break;
+  case 'run':
+    // expect npm start -- run scriptName overrideURL(optional)
+    // if scriptName exists, execute runScript with overrideURL
+    if (scriptName) {
+      app.listen(3999, () => console.log('kondo connected to server on port 3999'));
+      action.runScript(scriptName, inputURL);
+    }
+    // else print syntax explanation
+    else console.log("Please enter the name of the script you'd like to run")
+    break;
+  default:
+    console.log('Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"')
 }
 
 //route handling
@@ -71,5 +47,3 @@ app.get('/index', (req, res) => {
 app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 })
-
-module.exports = app;
