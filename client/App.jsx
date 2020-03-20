86c321f6be7f943ca84c82f044c82ce430d9cbc2
cychoi00 <yenachoi95@gmail.com>
2020-03-19 18:30:28 -0700

    changing branches

--- client/App.jsx
+++ client/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 // const inputURL = process.argv.slice(2)[0];
 
-const App = props => {
+const App = () => {
   return (
     <div className="appComponent">
       {/* <p>{inputURL}</p> */}
       {/* <Wrapper /> */}
+      <p>text</p>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -7,39 +7,39 @@ const scripts = require('./userscripts');
 const app = express();
 const PORT = 3000;
 
-console.log(process.argv);
-// take the URL to open in Puppeteer from the input script parameter
-const inputURL = process.argv.slice(2)[0];
+// console.log(process.argv);
+// // take the URL to open in Puppeteer from the input script parameter
+// const inputURL = process.argv.slice(2)[0];
 
-// determines whether to get a new script or run an existing one in headless mode
-const runMode = process.argv.slice(3)[0];
+// // determines whether to get a new script or run an existing one in headless mode
+// const runMode = process.argv.slice(3)[0];
 
-// handle input parameters
-if (!inputURL)
-  console.log(
-    'Please provide a URL to connect to e.g. npm start -- http://localhost:3000 <scriptName(optional)>',
-  );
-else if (!runMode) CLI.addScript(inputURL);
-else if (runMode === '?') console.log(Object.keys(scripts));
-else {
-  // CLI.runScript(scripts[runMode], inputURL);
+// // handle input parameters
+// if (!inputURL)
+//   console.log(
+//     'Please provide a URL to connect to e.g. npm start -- http://localhost:3000 <scriptName(optional)>',
+//   );
+// else if (!runMode) CLI.addScript(inputURL);
+// else if (runMode === '?') console.log(Object.keys(scripts));
+// else {
+// CLI.runScript(scripts[runMode], inputURL);
 
-  // start server --> route handling
-  app.get('/client/index.js', (req, res) => {
-    console.log('sent index.js');
-    res.sendFile(path.join(__dirname, '../client/index.js'));
-  });
+// start server --> route handling
+app.use('/build', express.static(path.join(__dirname, '../build')));
 
-  app.use('/', (req, res) => {
-    console.log('sent index.html');
-    res.sendFile(path.join(__dirname, '../index.html'));
-  });
+// app.get('/client/index.js', (req, res) => {
+//   console.log('sent index.js');
+//   res.sendFile(path.join(__dirname, '../client/index.js'));
+// });
 
-  // app.use('/', express.static('../index.html'));
+app.get('/', (req, res) => {
+  console.log('sent index.html');
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
-  app.listen(PORT, () => {
-    console.log(`kondo listening on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`kondo listening on port ${PORT}`);
+});
+// }
 
 module.exports = app;
