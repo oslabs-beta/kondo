994d4ff2cefbc53b40e8d6c4d8f2e20301c9a1be
cychoi00 <yenachoi95@gmail.com>
2020-03-23 01:41:10 -0700

    server merge/cli refactored

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-// const inputURL = process.argv.slice(2)[0];
+import Loading from './components/loading';
+// import Wrapper from './components/wrapper';
 
 const App = () => {
   return (
-    <div className="appComponent">
-      {/* <p>{inputURL}</p> */}
-      {/* <Wrapper /> */}
-      <p>text</p>
+    <div className="router">
+      <main>
+        <Switch>
+          <Route exact path="/" component={Loading} />
+          {/* <Route exact path="/analytics" component={Wrapper} /> */}
+        </Switch>
+      </main>
     </div>
   );
 };
