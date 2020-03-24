749965082d9c73a2ec3fe2bb3e184484287ff00f
cychoi00 <yenachoi95@gmail.com>
2020-03-24 15:52:48 -0700

    minor cleanup to staging to push to master

--- server/server.js
+++ server/server.js
@@ -1,63 +1,70 @@
 #!/usr/bin/env node
 const express = require('express');
-const app = express();
 const path = require('path');
 const action = require('./controllers/scriptController');
 
-const PORT = 3999;
+const app = express();
+const PORT = 8000;
 
 // determines whether to save a new script or run an existing one in headless mode
-let runMode = process.argv.slice(2)[0];
+const runMode = process.argv.slice(2)[0];
 
 // the name of the script to be saved or executed
-let scriptName = process.argv.slice(3)[0];
+const scriptName = process.argv.slice(3)[0];
 
 // take the URL to open in Puppeteer from the input script parameter
-let inputURL = process.argv.slice(4)[0];
-
-// static files
-app.use('/assets', express.static(path.join(__dirname, '../assets')));
-
-app.post('/code', (req, res) => {
-  console.log('REQUEST BODY: ' + req.body);
-})
-
-app.get('/analytics', (req, res) => {
-  // add middleware functions
-  res.sendStatus(200);
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+const inputURL = process.argv.slice(4)[0];
 
 // handle input parameters
 if (!runMode) {
-  console.log('Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"');
-}
-else switch (runMode) {
-  case 'create':
-    // expect npm start -- create scriptName URL
-    // if scriptName and URL exist, run createScript and write to userscripts.js
-    if (scriptName && inputURL) {
-      app.listen(PORT, () => console.log('kondo listening on port ' + PORT));
-      action.createScript(scriptName, inputURL);
-    }
-    // else print syntax explanation
-    else console.log("Please enter the name of the script you'd like to create followed by the page URL.")
-    break;
-  case 'run':
-    // expect npm start -- run scriptName overrideURL(optional)
-    // if scriptName exists, execute runScript with overrideURL
-    if (scriptName) {
-      app.listen(PORT, () => console.log('kondo listening on port ' + PORT));
-      action.runScript(scriptName, inputURL);
-    }
-    // else print syntax explanation
-    else console.log("Please enter the name of the script you'd like to run")
-    break;
-  default:
-    console.log('Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"')
-}
+  console.log(
+    'Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"',
+  );
+} else
+  switch (runMode) {
+    case 'create':
+      // expect npm start -- create scriptName URL
+      // if scriptName and URL exist, run createScript and write to userscripts.js
+      if (scriptName && inputURL) {
+        app.listen(PORT, () => console.log('kondo listening on port ' + PORT));
+        action.createScript(scriptName, inputURL);
+      }
+      // else print syntax explanation
+      else
+        console.log(
+          "Please enter the name of the script you'd like to create followed by the page URL.",
+        );
+      break;
+    case 'run':
+      // expect npm start -- run scriptName overrideURL(optional)
+      // if scriptName exists, execute runScript with overrideURL
+      if (scriptName) {
+        // static files
+        app.use('/assets', express.static(path.join(__dirname, '../assets')));
+
+        app.post('/code', (req, res) => {
+          console.log('REQUEST BODY: ' + req.body);
+        });
+
+        app.get('/analytics', (req, res) => {
+          // add middleware functions
+          res.sendStatus(200);
+        });
+
+        app.get('/', (req, res) => {
+          res.sendFile(path.join(__dirname, '../index.html'));
+        });
+
+        app.listen(PORT, () => console.log('kondo listening on port ' + PORT));
+        action.runScript(scriptName, inputURL);
+      }
+      // else print syntax explanation
+      else console.log("Please enter the name of the script you'd like to run");
+      break;
+    default:
+      console.log(
+        'Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"',
+      );
+  }
 
 module.exports = app;
