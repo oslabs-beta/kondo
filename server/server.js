ba44e06dcc9c9a38347e93bc1165dbefbfad5506
cychoi00 <yenachoi95@gmail.com>
2020-03-23 21:27:43 -0700

    creating new branch

--- server/server.js
+++ server/server.js
@@ -14,32 +14,32 @@ const inputURL = process.argv.slice(2)[0];
 const runMode = process.argv.slice(3)[0];
 
 // handle input parameters
-if (!inputURL)
-  console.log(
-    'Please provide a URL to connect to e.g. npm start -- http://localhost:3000 <scriptName(optional)>',
-  );
-else if (!runMode) CLI.addScript(inputURL);
-else if (runMode === '?') console.log(Object.keys(scripts));
-else {
-  CLI.runScript(scripts[runMode], inputURL);
+// if (!inputURL)
+//   console.log(
+//     'Please provide a URL to connect to e.g. npm start -- http://localhost:3000 <scriptName(optional)>',
+//   );
+// else if (!runMode) CLI.addScript(inputURL);
+// else if (runMode === '?') console.log(Object.keys(scripts));
+// else {
+//   CLI.runScript(scripts[runMode], inputURL);
 
-  // ***START SERVER - ROUTE HANDLING***
+// ***START SERVER - ROUTE HANDLING***
 
-  // static files
-  app.use('/assets', express.static(path.join(__dirname, '../assets')));
+// static files
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
-  app.get('/analytics', (req, res) => {
-    // add middleware functions
-    res.sendStatus(200);
-  });
+app.get('/analytics', (req, res) => {
+  // add middleware functions
+  res.sendStatus(200);
+});
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
-  app.listen(PORT, () => {
-    console.log(`kondo listening on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`kondo listening on port ${PORT}`);
+});
+// }
 
 module.exports = app;
