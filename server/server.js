f0259013b5589c51966d9d541ddaaebe26759c34
cychoi00 <yenachoi95@gmail.com>
2020-03-25 15:50:54 -0700

    commit to pull

--- server/server.js
+++ server/server.js
@@ -16,29 +16,29 @@ const scriptName = process.argv.slice(3)[0];
 const inputURL = process.argv.slice(4)[0];
 
 // handle input parameters
-// if (!runMode) {
-//   console.log(
-//     'Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"',
-//   );
-// } else
-//   switch (runMode) {
-//     case 'create':
-//       // expect npm start -- create scriptName URL
-//       // if scriptName and URL exist, run createScript and write to userscripts.js
-//       if (scriptName && inputURL) {
-//         app.listen(PORT, () => console.log('kondo listening on port ' + PORT));
-//         action.createScript(scriptName, inputURL);
-//       }
-//       // else print syntax explanation
-//       else
-//         console.log(
-//           "Please enter the name of the script you'd like to create followed by the page URL.",
-//         );
-//       break;
-//     case 'run':
+if (!runMode) {
+  console.log(
+    'Please enter "create" to make a new script or "run" to execute an existing one e.g. "npm start -- create/run scriptName url"',
+  );
+} else
+  switch (runMode) {
+    case 'create':
+      // expect npm start -- create scriptName URL
+      // if scriptName and URL exist, run createScript and write to userscripts.js
+      if (scriptName && inputURL) {
+        app.listen(PORT, () => console.log('kondo listening on port ' + PORT));
+        action.createScript(scriptName, inputURL);
+      }
+      // else print syntax explanation
+      else
+        console.log(
+          "Please enter the name of the script you'd like to create followed by the page URL.",
+        );
+      break;
+    case 'run':
 // expect npm start -- run scriptName overrideURL(optional)
 // if scriptName exists, execute runScript with overrideURL
-// if (scriptName) {
+if (scriptName) {
 // static files
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
