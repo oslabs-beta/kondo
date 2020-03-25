10149507474d62a39c032f8d855514601ee6028f
Elizabeth Soto <elizabeths87@icloud.com>
2020-03-25 14:54:45 -0700

    changes

---
+++ client/components/barGraph.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import React, { Bar } from 'react-chartjs-2'
+
+class Chart extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            chartData: {
+                labels: ['San Francisco', 'Los Angeles', 'San Diego', 'Modesto'],
+                datasets: [{
+                    label: 'Population',
+                    data: [ 870044,
+                        3959657,
+                        1401932,
+                        211336 ]
+                }]
+            }
+        }
+    }
+    render() {
+        return (
+        <div className='chart'>
+            <Bar
+            data={this.state.data}
+            options={{ }} />
+            
+        </div>
+        )
+    }
+}
+
+export default Chart
