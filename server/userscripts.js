2233e254d804a6885874a21dbdac290ad3d6d23a
andieritter <58995450+andieritter@users.noreply.github.com>
2020-03-26 15:18:24 -0700

    cleared test script from userscripts

--- server/userscripts.js
+++ server/userscripts.js
@@ -7,11 +7,12 @@ exports.startStop = {
     await page.click('body > #stop-button');
   },
 };
-exports.start = { 
-    url: 'http://localhost:8080',
-    func: async (page) => { 
-  await page.waitForSelector('body > #leak-button');  await page.click('body > #leak-button');  
-  } 
+exports.start = {
+  url: 'http://localhost:8080',
+  func: async (page) => {
+    await page.waitForSelector('body > #leak-button'); await page.click('body > #leak-button');
   }
-    
-  
+}
+
+
+
