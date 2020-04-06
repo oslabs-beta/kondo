e50b6ea9a66a2a9b3095b3d29c1b8ff2a3c1256a
MrAustin2u <aaustin86@gmail.com>
2020-04-06 11:35:49 -0700

    Database schema complete

--- server/models/models.js
+++ server/models/models.js
@@ -1,25 +1,27 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config({ path: __dirname + "/../../.env" });
-
+const dotenv = require("dotenv").config();
+const Schema = mongoose.Schema;
 const MONGO_URI = `mongodb+srv://${process.env.PASSWORD}@cluster0-donuw.mongodb.net/test?retryWrites=true&w=majority`;
+
+console.log(process.env.PASSWORD);
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "kondo"
+    dbName: "kondo",
   })
   .then(() => {
     console.log("connected to kondo database");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
 
 //sets a schema for the 'users'
 const usersSchema = new Schema({
   username: String,
-  password: String
+  password: String,
   //sites: ARRAY of Objects
 });
 
@@ -30,12 +32,13 @@ const sitesSchema = new Schema({
   url: String,
   siteName: String,
   scripts: [String],
-  runs: [Object]
+  runs: [{}],
+  date: { type: Date, default: Date.now },
 });
 
 const Sites = mongoose.model("sites", sitesSchema);
 
 module.exports = {
   Users,
-  Sites
-}
+  Sites,
+};
