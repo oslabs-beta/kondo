5685c264a0a203477c818a70b288e944a994a1b5
andieritter <58995450+andieritter@users.noreply.github.com>
2020-04-06 14:07:07 -0700

    merged in new changes from staging, app works now

--- server/userscripts.js
+++ server/userscripts.js
@@ -1,21 +1,7 @@
-<<<<<<< HEAD
-exports.startStop = {
-  url: 'http://localhost:8080',
-  func: async page => {
-    await page.waitForSelector('body > #leak-button');
-    await page.click('body > #leak-button');
-    await page.waitForSelector('body > #stop-button');
-    await page.click('body > #stop-button');
-  },
-};
-exports.start = {
-  url: 'http://localhost:8080',
-  func: async (page) => {
-    await page.waitForSelector('body > #leak-button'); await page.click('body > #leak-button');
+exports.newScript = {
+  url: 'http://127.0.0.1:5500/index.html',
+  func: async (page) => {  
+  await page.waitForSelector('body > #leak-button');  await page.click('body > #leak-button');  
   }
 }
 
-
-
-=======
->>>>>>> cd5d123d2b6a839d24075f532a616fc6fe0f5cd6
