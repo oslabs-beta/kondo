630e7e2ff4ece6b26e34f23655657ba43d2817c4
andieritter <58995450+andieritter@users.noreply.github.com>
2020-04-07 12:31:55 -0700

    added middleware that posts new user script to db instead of userscripts file

--- server/controllers/scriptController.js
+++ server/controllers/scriptController.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const scriptName = process.argv[3];
 const inputURL = process.argv[4];
-const { Runs } = require("../models/models");
+const { Runs, Scripts } = require("../models/models");
 
 const trimScript = (input) => {
   // remove first two lines containing URL and viewport info, replace blank lines with semicolons
@@ -18,44 +18,39 @@ const trimScript = (input) => {
 
 const scriptController = {};
 
-scriptController.postHeaps = (req, res, next) => {
-  console.log(`IN postHeaps`);
-  Runs.create(
+scriptController.postScript = (req, res, next) => {
+  Scripts.create(
     {
-      heapUsageOverTime: req.body.heapUsageOverTime,
-      memoryLeaks: req.body.memoryLeaks,
+      name: scriptName,
+      script: `${req.body.code}`,
     },
-    (err, postHeap) => {
-      if (err) {
-        console.log(`Error in databaseController.postHeaps`);
-        res.sendStatus(418);
-        return next();
-      } else {
-        console.log(`I am inside of else statement for postHeaps`);
-        res.locals.heapUsageOverTime = postHeap.heapUsageOverTime;
-        res.locals.memoryLeaks = postHeap.memoryLeaks;
-        return next();
-      }
-    }
-  );
-};
-
-scriptController.storeScript = (req, res, next) => {
-  // call helper function to process and return new script string
-  let newScript = trimScript(req.body.code);
-  // add the object locally to the userscripts file
-  fs.appendFile(
-    path.join(__dirname, "../userscripts.js"),
-    newScript,
-    "utf-8",
-    function(err) {
-      if (err) next(err);
+    (err, scriptData) => {
+      if (err) return next(err);
       console.log(
         `Saved successfully! You can run this test by entering "npm start -- run ${scriptName}"`
       );
-      next();
+      return next();
     }
-  );
-};
+  )
+}
+
+// scriptController.storeScript = (req, res, next) => {
+//   // call helper function to process and return new script string
+//   console.log('script:' + req.body.code);
+//   let newScript = trimScript(req.body.code);
+//   // add the object locally to the userscripts file
+//   fs.appendFile(
+//     path.join(__dirname, "../userscripts.js"),
+//     newScript,
+//     "utf-8",
+//     function (err) {
+//       if (err) next(err);
+//       console.log(
+//         `Saved successfully! You can run this test by entering "npm start -- run ${scriptName}"`
+//       );
+//       next();
+//     }
+//   );
+// };
 
 module.exports = scriptController;
