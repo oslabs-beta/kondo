bb9fbe9ec06da5ff3c651dcb4d2a6bb479816020
andieritter <58995450+andieritter@users.noreply.github.com>
2020-04-08 23:38:31 -0700

    migrated code out of server.js and into createScript.js

--- server/server.js
+++ server/server.js
@@ -1,38 +1,9 @@
 const express = require("express");
 const path = require("path");
-const { scriptController } = require("./controllers/scriptController");
-const { createScript } = require("./createScript");
+const scriptController = require("./controllers/scriptController");
 const heapController = require("./controllers/heapController");
 const app = express();
-const PORT = 8000;
-
-// determines whether to save a new script or run an existing one in headless mode
-const runMode = process.argv[2];
-const scriptName = process.argv[3];
-
-// take the URL to open in Puppeteer from the input script parameter
-let inputURL;
-if (process.argv[4]) {
-  inputURL = process.argv[4];
-}
-
-// handle input parameters
-if (!runMode) {
-  console.log(
-    'Please enter "npm start -- create scriptName url" to create a new script, or "npm start -- run scriptName" to run an existing one.'
-  );
-} else {
-  app.listen(PORT, () => console.log('Kondo is listening on port ' + PORT));
-  // expects 'npm start -- create scriptName URL' or 'npm start -- run scriptName'
-  if (runMode == 'create') {
-    createScript(scriptName, inputURL);
-  } else if (runMode != 'run') {
-    console.log(
-      'Please enter "npm start -- create scriptName url" to create a new script, or "npm start -- run scriptName" to run an existing one.',
-    );
-    process.exit(0);
-  }
-}
+const PORT = 8001;
 
 // *** ERROR HANDLING *** //
 function logErrors(err, req, res, next) {
@@ -51,11 +22,11 @@ app.use(express.json());
 
 app.use("/assets", express.static(path.join(__dirname, "../assets")));
 
-app.post("/code", scriptController.storeScript, /*heapController.postHeap,*/(req, res) => {
-  res.status(200).send("OK");
-  process.exit(0);
-}
-);
+// app.post("/code", scriptController.storeScript, /*heapController.postHeap,*/(req, res) => {
+//   res.status(200).send("OK");
+//   process.exit(0);
+// }
+// );
 
 app.get("/data", heapController.getData, (req, res) => {
   res.json(res.locals);
@@ -66,4 +37,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.post;
+// app.post;
