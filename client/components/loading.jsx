77f88f913846954c66dd44831356b6359b45a3b8
cychoi00 <yenachoi95@gmail.com>
2020-04-09 12:03:00 -0700

    updated to allow for text animation

--- client/components/loading.jsx
+++ client/components/loading.jsx
@@ -5,30 +5,31 @@ const Loading = props => {
   const [analysisComplete, setLoadingStatus] = useState(false);
 
   useEffect(() => {
-    $('body').ripples({ resolution: 256, refraction: 10, dropRadius: 60 });
-    const textWrapper = document.querySelector('.ml9 .letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(
-      /\S/g,
-      "<span class='letter'>$&</span>",
-    );
+    if (!analysisComplete) {
+      $('body').ripples({ resolution: 256, refraction: 10, dropRadius: 60 });
+      const textWrapper = document.querySelector('.ml9 .letters');
+      textWrapper.innerHTML = textWrapper.textContent.replace(
+        /\S/g,
+        "<span class='letter'>$&</span>",
+      );
 
-    anime
-      .timeline({ loop: true })
-      .add({
-        targets: '.ml9 .letter',
-        scale: [0, 1],
-        duration: 1500,
-        elasticity: 600,
-        delay: (el, i) => 45 * (i + 1),
-      })
-      .add({
-        targets: '.ml9',
-        opacity: 0,
-        duration: 1000,
-        easing: 'easeOutExpo',
-        delay: 1000,
-      });
-
+      anime
+        .timeline({ loop: true })
+        .add({
+          targets: '.ml9 .letter',
+          scale: [0, 1],
+          duration: 1500,
+          elasticity: 600,
+          delay: (el, i) => 45 * (i + 1),
+        })
+        .add({
+          targets: '.ml9',
+          opacity: 0,
+          duration: 1000,
+          easing: 'easeOutExpo',
+          delay: 1000,
+        });
+    }
     fetch('/data')
       .then(res => res.json())
       .then(data => {
@@ -38,6 +39,7 @@ const Loading = props => {
   });
 
   if (analysisComplete) {
+    console.log('redirecting');
     return <Redirect to="/analytics" />;
   }
   return (
--- server/runScript.js
+++ server/runScript.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
-const scriptController = require("./controllers/scriptController");
-const heapController = require("./controllers/heapController");
+const express = require('express');
+const path = require('path');
+const scriptController = require('./controllers/scriptController');
+const heapController = require('./controllers/heapController');
 const app = express();
 const PORT = 8000;
 
 // *** ERROR HANDLING *** //
 function logErrors(err, req, res, next) {
   const defaultErr = {
-    log: "Express error handler caught unknown middleware error",
+    log: 'Express error handler caught unknown middleware error',
     status: 400,
-    message: { err: "An error occurred" },
+    message: { err: 'An error occurred' },
   };
   const errorObj = Object.assign({}, defaultErr);
   console.log(errorObj.log);
@@ -20,18 +20,18 @@ function logErrors(err, req, res, next) {
 // *** SERVER ROUTES *** //
 app.use(express.json());
 
-app.use("/assets", express.static(path.join(__dirname, "../assets")));
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
-app.get("/data", heapController.getData, (req, res) => {
+app.get('/data', heapController.getData, (req, res) => {
   res.json(res.locals);
   process.exit(0);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Kondo is listening on port ${PORT}`);
-})
+});
 // app.post;
